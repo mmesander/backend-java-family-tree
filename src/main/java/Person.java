21c793fea0b9a3dc079665f75b5b68dfbc2abe5c
mmesander <mark_mesander@hotmail.com>
2023-10-12 17:21:57 +0200

    add variables and constructor

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,27 @@
+import java.util.List;
+
 public class Person {
+    // Variables
+    private String name;
+    private String middleName;
+    private String lastName;
+    private char sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List siblings;
+    private List children;
+    private List pets;
+
+
+    // Constructors
+    public Person(String name, String lastName, int age, char sex) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+
+
 }
